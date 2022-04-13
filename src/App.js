bf3c61cb9face19ac941e4d355e31546e84244e0
ohziiilov3 <klbaskerville0520@gmail.com>
2022-04-13 16:58:40 -0700

    Fixed Header added Container and update Navbar to list products dropdown

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import {Container} from 'react-bootstrap'
 
 function App() {
   return (
     <div>
     <Header />
      <main className="py-5">
-       <header>
+       <header >
+        <Container>
       <h1>Welcome</h1>
+      </Container>
       </header>    
       </main>
     <Footer />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,28 @@
 import React from 'react'
-import { Navbar, Nav, Container, Input, Button,} from 'react-bootstrap'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 
 function Header() {
     return (
-       <header>    
-     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Swift Stitch Upholstery</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarColor02">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Home
-            <span class="visually-hidden">(current)</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About Us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Products</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact Us</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+        <header>    
+     <Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">Swift Stitch Upholstery</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">About Us</Nav.Link>
+        <NavDropdown title="Products" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Textiles</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Fabric</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Thread</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Leather</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
         </header>
 
     )
