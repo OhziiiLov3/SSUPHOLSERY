f2b7a29acb1d4785ef9e00a34b7445f206cc1f1d
ohziiilov3 <klbaskerville0520@gmail.com>
2022-04-29 12:46:22 -0700

    products displaying on product show page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import logo from "../images/PrimaryIcon.png";
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <header>    
      <Navbar bg="dark" variant="dark" expand="lg" collaspeonselect="true">
   <Container>
-    <Navbar.Brand href="/">Swift Stitch Upholstery</Navbar.Brand>
+    <LinkContainer to='/'>
+    <Navbar.Brand>Swift Stitch Upholstery</Navbar.Brand>
+  </LinkContainer>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
+        <LinkContainer to='/'>
+        <Nav.Link>Home</Nav.Link>
+        </LinkContainer>
         <Nav.Link href="#link">About Us</Nav.Link>
+
         <NavDropdown title="Products" id="basic-nav-dropdown">
           <NavDropdown.Item href="#action/3.1">Textiles</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.2">Needles</NavDropdown.Item>
@@ -21,8 +28,15 @@ function Header() {
           <NavDropdown.Item href="#action/3.4">Leather</NavDropdown.Item>
         </NavDropdown>
         <a className="navbar-brand" href="/"><img className="logo" src={logo} alt="logo..." /></a>
-        <Nav.Link href="#link"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
-        <Nav.Link href="#link"><i className="fas fa-user"></i>Login</Nav.Link>
+
+        <LinkContainer to='/cart'>
+        <Nav.Link><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+        </LinkContainer>
+        
+        <LinkContainer to='/login'>
+        <Nav.Link><i className="fas fa-user"></i>Login</Nav.Link>
+        </LinkContainer>
+
       </Nav>
     </Navbar.Collapse>
   </Container>
--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import {Link, useParams  } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
 function ProductScreen() {
+    const params = useParams();
+    const product = products.find((p) => p._id == params.id)
     return (
         <div>
-          Products  
+            <Link to='/' className='btn btn-light my-3'>Go Back</Link> 
+            <Row>
+                <Col md={6}>
+                    <Image src={product.image} alt={product.name} fluid/>
+                </Col>
+            </Row>
         </div>
     )
 }
