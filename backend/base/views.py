394c6903eb853771e2282408ef65c681dc6f6eea
ohziiilov3 <klbaskerville0520@gmail.com>
2022-06-10 10:41:49 -0700

    added django app, django server working

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
+# URLS- Funtion base views allow to logic within the view (Class are more optimal)
+def getRoutes(request):
+    return JsonResponse('Hello', safe=False)
