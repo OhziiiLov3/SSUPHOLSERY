a42a34de5d70f1f6d1bd2225327b8bc8774e0cbf
ohziiilov3 <klbaskerville0520@gmail.com>
2022-06-14 09:26:17 -0700

    added models

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Service
+from .models import *
 
 # Register your models here.
 admin.site.register(Service)
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
